f70fda1b50dc83f898b92fbe7a2fdafc4c0a9100
viktorria2 <124798603+viktorria2@users.noreply.github.com>
2024-11-27 12:06:00 +0100

    Update SSD1306.h

--- digital2_project_screen/lib/SSD1306/SSD1306.h
+++ digital2_project_screen/lib/SSD1306/SSD1306.h
@@ -1,1 +1,17 @@
+#ifndef SSD1306_H
+#define SSD1306_H
 
+#include <Wire.h>
+#include <Adafruit_SSD1306.h>
+
+#define SCREEN_WIDTH 128
+#define SCREEN_HEIGHT 64
+#define OLED_RESET    -1
+#define OLED_ADDR     0x3C
+
+void initDisplay();
+void clearDisplay();
+void printNote(const char* note);
+void setCursor(uint8_t x, uint8_t y);
+
+#endif
