9597c740fa2d14f002bc4001fdb8c811995b4a94
viktorria2 <124798603+viktorria2@users.noreply.github.com>
2024-11-27 12:11:35 +0100

    Update SSD1306.h

--- digital2_project_screen/lib/SSD1306/SSD1306.h
+++ digital2_project_screen/lib/SSD1306/SSD1306.h
@@ -8,10 +8,17 @@
 #define SCREEN_HEIGHT 64
 #define OLED_RESET    -1
 #define OLED_ADDR     0x3C
+// button pins
+#define BUTTON_START_PIN 2
+#define BUTTON_STOP_PIN  3
 
+//functions
 void initDisplay();
 void clearDisplay();
 void printNote(const char* note);
 void setCursor(uint8_t x, uint8_t y);
+void initButtons();
+unsigned long getElapsedTime();
+void displayElapsedTime(unsigned long elapsedTime);
 
 #endif
