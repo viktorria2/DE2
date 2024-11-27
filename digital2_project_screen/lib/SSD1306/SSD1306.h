a79adb3afb74ba4063e4c52b2d1d6036d89e0cb4
viktorria2 <124798603+viktorria2@users.noreply.github.com>
2024-11-27 12:32:51 +0100

    Update SSD1306.h

--- digital2_project_screen/lib/SSD1306/SSD1306.h
+++ digital2_project_screen/lib/SSD1306/SSD1306.h
@@ -2,12 +2,12 @@
 #define SSD1306_H
 
 #include <Wire.h>
-#include <Adafruit_SSD1306.h>
+#include <U8g2lib.h>
 
 #define SCREEN_WIDTH 128
 #define SCREEN_HEIGHT 64
-#define OLED_RESET    -1
-#define OLED_ADDR     0x3C
+//#define OLED_RESET    -1
+//#define OLED_ADDR     0x3C
 // button pins
 #define BUTTON_START_PIN 2
 #define BUTTON_STOP_PIN  3
@@ -18,7 +18,7 @@ void clearDisplay();
 void printNote(const char* note);
 void setCursor(uint8_t x, uint8_t y);
 void initButtons();
-unsigned long getElapsedTime();
-void displayElapsedTime(unsigned long elapsedTime);
+unsigned long getmeasuredTime();
+void displaymeasuredTime(unsigned long measuredTime);
 
 #endif
